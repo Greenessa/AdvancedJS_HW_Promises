6b0ce06a3d5fba9afa51dc8d3d7f93a6a2064145
Margo <romanova184@yandex.ru>
2025-05-05 11:41:53 +0300

    Second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,5 @@
 // TODO: write your code here
 import GameSavingLoader from './object';
 
-GameSavingLoader.load().then((saving) => {
-    // saving объект класса GameSaving
-    console.log(saving);
-  }, (error) => {
-    console.log(`Ошибка: ${error.message}`)
-  });
+GameSavingLoader.load();
+  
--- src/js/object.js
+++ src/js/object.js
@@ -1,21 +1,14 @@
 import read from "./reader";
 import json from "./parser";
 
-// export default class GameSavingLoader {
-//   static load() {
-//     const data = read(); // возвращается Promise!
-//     const value = json(data); // возвращается Promise!
-//     return value;
-//   }
-// }
-
 export default class GameSavingLoader {
   static load() {
-    return new Promise(function(resolve, reject) {
-    const data = read(); // возвращается Promise!
-    const value = json(data); // возвращается Promise!
-    console.log(value);
-    resolve(value);
-    reject(new Error('Ошибка загрузки данных'));
-  })}
+    read().then((data) => json(data))
+    .then((value) => {
+      console.log(value);
+      return value
+    }).catch((error) => console.log('Файл не загрузился'))
+  }
 }
+
+
